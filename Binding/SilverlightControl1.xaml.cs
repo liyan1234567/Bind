f8a57e8e588fdca998c4532b2f2073f3a55d688d
li.yan <18729347300@163.com>
2019-01-19 02:09:11 +0800

    Binding

---
+++ Binding/SilverlightControl1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Binding
+{
+    public partial class SilverlightControl1 : UserControl
+    {
+        person p = new person() { Name="张三",Sex=true,Age=12};
+        public SilverlightControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void usercontrol_Loaded(object sender, RoutedEventArgs e)
+        {
+            li.DataContext = p;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(p.Name);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            p.Name = "lisi";
+        }
+
+    }
+}
---
+++ Binding/person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Binding
+{
+    public class person:DependencyObject
+    {
+        
+        public string Name
+        {
+            get { return GetValue(NameProperty).ToString(); }
+            set { SetValue(NameProperty,value); }
+        }
+
+        public static DependencyProperty NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(person), null);
+      
+        bool sex;
+
+        public bool Sex
+        {
+            get { return sex; }
+            set { sex = value; }
+        }
+        int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
